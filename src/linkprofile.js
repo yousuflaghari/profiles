21a31f591b89a536dbd1d7b9fed409e2b8de08bf
yousuf <yousuflaghari786@gmail.com>
2024-03-27 19:34:02 +0500

    css add

--- src/linkprofile.js
+++ src/linkprofile.js
@@ -7,14 +7,10 @@ const Linkprofile = ({ profiles }) => {
   const profile = profiles[index];
 
   return (
-    <div className="container1">
+   <div className="profilemain">
+     <div className="container1">
       <h1 className="h1">Profile</h1>
       <h6 className="h3">I'm a creative webdeveloper</h6>
-      <img
-        src={`https://api.dicebear.com/8.x/pixel-art/svg?seed=${profile.firstname}`}
-        alt={`Profile ${index}`}
-        className="img"
-      />
       <div className="container2">
         <div className="details">
           <h3 className="headingdetail">Details</h3>
@@ -32,16 +28,22 @@ const Linkprofile = ({ profiles }) => {
             through code.</p>
         </div>
         <div className="avatar">
+          <img
+            src={`https://api.dicebear.com/8.x/pixel-art/svg?seed=${profile.firstname}`}
+            alt={`Profile ${index}`}
+            className="img1"
+          />
           <div className="avatardiv">
-          <h3 className="h4">HELLO I'M</h3>
-          <h3 className="h4">{profile.firstname}</h3>
-        <p className="paragraph1">As a software engineer, you are at the forefront of technological
-            innovation, wielding the power to shape the digital landscape and solve complex problems
-            through code.</p>
+            <h3 className="h4">HELLO I'M</h3>
+            <h3 className="h4">{profile.firstname}</h3>
+            <p className="paragraph1">As a software engineer, you are at the forefront of technological
+              innovation, wielding the power to shape the digital landscape and solve complex problems
+              through code.</p>
           </div>
         </div>
       </div>
     </div>
+   </div>
   );
 };
 
