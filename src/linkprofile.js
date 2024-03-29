3d59fef9654ce52d5de7405a28c7d7bd038dfa51
yousuf <yousuflaghari786@gmail.com>
2024-03-29 10:10:54 +0500

    first commit

--- src/linkprofile.js
+++ src/linkprofile.js
@@ -1,49 +1,155 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import "./linkprofile.css"
+import "./linkprofile.css";
+
 const Linkprofile = ({ profiles }) => {
   const { index } = useParams();
-  console.log(index)
   const profile = profiles[index];
+  const [firstName, setFirstName] = useState(profile.first_name);
+  const [email, setEmail] = useState(profile.email);
+  const [website, setWebsite] = useState(profile.website);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleWebsiteChange = (e) => {
+    setWebsite(e.target.value);
+  };
+
+  const handleeditname = () => {
+    setEditMode(true);
+  };
+  const handleeditemail = () => {
+    setEditMode(true);
+  };
+  const handleeditwebsite = () => {
+    setEditMode(true);
+  };
+
+  const handlesavename = () => {
+    setEditMode(false);
+  };
+  const handlesaveemail = () => {
+    setEditMode(false);
+  };
+  const handlesavewebsite = () => {
+    setEditMode(false);
+  };
+
+  const handlecancelname = () => {
+    setFirstName(profile.first_name);
+    setEditMode(false);
+  };
+  const handlecancelemail = () => {
+    setEmail(profile.email);
+    setEditMode(false);
+  };
+  const handlecancelwebsite = () => {
+   setWebsite(profile.website);
+    setEditMode(false);
+  };
 
   return (
-   <div className="profilemain">
-     <div className="container1">
-      <h1 className="h1">Profile</h1>
-      <h6 className="h3">I'm a creative webdeveloper</h6>
-      <div className="container2">
-        <div className="details">
-          <h3 className="headingdetail">Details</h3>
-          <h6 className="firstName">firstName</h6>
-          <p className="firstNamep">{profile.firstname}</p>
-          <h6 className="firstName">Email</h6>
-          <p className="firstNamep">{profile.email}</p>
-          <h6 className="firstName">Website</h6>
-          <p className="firstNamep">{profile.website}</p>
-        </div>
-        <div className="aboutme">
-          <h3 className="headingaboutme">About me</h3>
-          <p className="paragraph">As a software engineer, you are at the forefront of technological
-            innovation, wielding the power to shape the digital landscape and solve complex problems
-            through code.</p>
-        </div>
-        <div className="avatar">
-          <img
-            src={`https://api.dicebear.com/8.x/pixel-art/svg?seed=${profile.firstname}`}
-            alt={`Profile ${index}`}
-            className="img1"
-          />
-          <div className="avatardiv">
-            <h3 className="h4">HELLO I'M</h3>
-            <h3 className="h4">{profile.firstname}</h3>
-            <p className="paragraph1">As a software engineer, you are at the forefront of technological
-              innovation, wielding the power to shape the digital landscape and solve complex problems
-              through code.</p>
+    <div className="profilemain">
+      <div className="container1">
+        <h1 className="h1">Profile</h1>
+        <h6 className="h3">I'm a creative webdeveloper</h6>
+        <div className="container2">
+          <div className="details">
+            <h3 className="headingdetail">Details</h3>
+            <h6 className="firstName">First Name</h6>
+            {editMode ? 
+              <input
+                type="text"
+                value={firstName}
+                onChange={handleFirstNameChange}
+              />
+             : 
+              <p className="firstNamep">{firstName}</p>
+            }
+            {editMode ? 
+              <div>
+                <button onClick={handlesavename}>Save</button>
+                <button onClick={handlecancelname}>Cancel</button>
+              </div>
+             : 
+              <div>
+                <button onClick={handleeditname}>Edit</button>
+                <button>Delete</button>
+              </div>
+            }
+            <h6 className="firstName">Email</h6>
+            {editMode ? 
+              <input
+                type="text"
+                value={email}
+                onChange={handleEmailChange}
+              />
+             : 
+              <p className="firstNamep">{email}</p>
+            }
+            {editMode ? 
+              <div>
+                <button onClick={handlesaveemail}>Save</button>
+                <button onClick={handlecancelemail}>Cancel</button>
+              </div>
+             : 
+              <div>
+                <button onClick={handleeditemail}>Edit</button>
+                <button>Delete</button>
+              </div>
+            }
+            <h6 className="firstName">Website</h6>
+            {editMode ? 
+              <input
+                type="text"
+                value={website}
+                onChange={handleWebsiteChange}
+              />
+             : 
+              <p className="firstNamep">{website}</p>
+            }
+            {editMode ? 
+              <div>
+                <button onClick={handlesavewebsite}>Save</button>
+                <button onClick={handlecancelwebsite}>Cancel</button>
+              </div>
+             : 
+              <div>
+                <button onClick={handleeditwebsite}>Edit</button>
+                <button>Delete</button>
+              </div>
+            }
+          </div>
+          <div className="aboutme">
+            <h3 className="headingaboutme">About me</h3>
+            <p className="paragraph">
+              As a software engineer, you are at the forefront of technological
+              innovation, wielding the power to shape the digital landscape and
+              solve complex problems through code.
+            </p>
+          </div>
+          <div className="avatar">
+            <img src={profile.image} alt={`Profile ${index}`} className="img1" />
+            <div className="avatardiv">
+              <h3 className="h4">HELLO I'M</h3>
+              <h3 className="h4">{firstName}</h3>
+              <p className="paragraph1">
+                As a software engineer, you are at the forefront of technological
+                innovation, wielding the power to shape the digital landscape and
+                solve complex problems through code.
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-   </div>
   );
 };
 
--- src/profile1.js
+++ src/profile1.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { faker } from "@faker-js/faker";
 import "./profile1.css";
 import { Link } from "react-router-dom";
 
@@ -7,13 +6,10 @@ const Profile1 = ({ profiles, setProfiles }) => {
     useEffect(() => {
         const fetchProfiles = async () => {
             try {
-                const userData = Array.from({ length: 9 }, () => ({
-                    firstname: faker.person.firstName(),
-                    email: faker.internet.email(),
-                    website: faker.internet.url(),
-                }));
-               
-                setProfiles(userData);
+                const Response = await fetch("https://6343e0272dadea1175af15e4.mockapi.io/users")
+                const data = await Response.json()
+                console.log(data)
+                setProfiles(data)
             } catch (error) {
                 console.error("Error fetching profiles:", error);
             }
@@ -24,7 +20,8 @@ const Profile1 = ({ profiles, setProfiles }) => {
     }, [setProfiles]);
 
     return (
-        <div className="main">
+        
+         <div className="main">
             <div className="container">
             <p className="p1">OUR TEAM MEMBERS</p>
             <p className="p2">MEET OUR TEAM!</p>
@@ -34,18 +31,19 @@ const Profile1 = ({ profiles, setProfiles }) => {
                     <Link to={`/profile/${index}`} key={index} className="link">
                         <div key={index} className="profile">
                             <img
-                                src={`https://api.dicebear.com/8.x/pixel-art/svg?seed=${profile.firstname}`}
+                                src={profile.image}
                                 alt={`Profile ${index}`}
                                 className="img"
                             />
-                            <div className="name">{profile.firstname}</div>
+                            <div className="name">{profile.first_name}</div>
                             <div className="email">{profile.email}</div>
                             <div className="website">{profile.website}</div>
                         </div>
                     </Link>
                 ))}
             </div>
-        </div>
+        </div> 
+    
     );
 };
 
